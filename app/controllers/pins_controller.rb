548a565ed80ff9840e26a413cefbed4d91909351
Gerard Dawson <gerard.dawson1@gmail.com>
2014-06-24 18:17:17 -0400

    Cleaned up models.pins file

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,9 +1,12 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @pins = Pin.all
   end
+
 
   def show
   end
@@ -18,23 +21,23 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
+      redirect_to @pin, notice: 'Pin was successfully created.' 
     else
-      render action: 'new'
+      render action: 'new' 
     end
   end
 
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
+      redirect_to @pin, notice: 'Pin was successfully updated.' 
     else
-      render action: 'edit'
+      render action: 'edit' 
     end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url
+    redirect_to pins_url 
   end
 
   private
